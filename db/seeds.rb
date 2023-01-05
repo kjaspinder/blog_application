f412bdf574053bd01b335c989494df5b2225ed8b
jaspinderkaur <jaspinder.kaur028@gmail.com>
2023-01-05 21:45:39 +0400

    Added devise for user registration and sessions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "jaspinder@example.com", password: "123456", password_confirmation: "123456")
+
+10.times do |x|
+  Post.create(title: "Title #{x}", body: "Body #{x} words created for a post", user_id: User.first.id)
+end
